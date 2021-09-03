a468d877ef0816e64ae4e79c70dbba1695eea6a2
mohammadhssn <mohammadhssnalizadeh78@gmail.com>
2021-09-03 22:25:07 +0430

    4_add image bird in branch level

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,12 @@ using UnityEngine.UI;
 
 public class Game : MonoBehaviour
 {
+    private GameObject _blueBird;
+    private GameObject _yellowBird;
+    [SerializeField] private List<GameObject> imageBirds;
+
+
+
     [SerializeField] private Text scoreText;
     [SerializeField] private int playerScore = 10;
     
@@ -12,13 +19,35 @@ public class Game : MonoBehaviour
     {
         scoreText.text = playerScore.ToString();
     }
-    
+
+    private void Update()
+    {
+        NextImageBirds();
+    }
+
     public void AddToScore(int score)
     {
         playerScore += score;
         scoreText.text = playerScore.ToString();
     }
     
+    void NextImageBirds()
+    {
+        _blueBird = GameObject.Find("BlueBird(Clone)");
+        _yellowBird = GameObject.Find("YellowBird(Clone)");
+
+        if (_blueBird)
+        {
+            Destroy(imageBirds[0]);
+            return;
+        }
+        else if (_yellowBird)
+        {
+            Destroy(imageBirds[1]);
+            return;
+        }
+
+    }
     
     
 }
